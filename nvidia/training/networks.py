7eefcf83070adfed9d71d9a1ff9fd88dc6a2bdb0
Sayantan Das <sayantandas30011998@gmail.com>
2021-11-10 23:31:13 -0500

    changed networks.py

--- nvidia/training/networks.py
+++ nvidia/training/networks.py
@@ -8,12 +8,12 @@
 
 import numpy as np
 import torch
-from torch_utils import misc
-from torch_utils import persistence
-from torch_utils.ops import conv2d_resample
-from torch_utils.ops import upfirdn2d
-from torch_utils.ops import bias_act
-from torch_utils.ops import fma
+from nvidia.torch_utils import misc
+from nvidia.torch_utils import persistence
+from nvidia.torch_utils.ops import conv2d_resample
+from nvidia.torch_utils.ops import upfirdn2d
+from nvidia.torch_utils.ops import bias_act
+from nvidia.torch_utils.ops import fma
 
 #----------------------------------------------------------------------------
 
